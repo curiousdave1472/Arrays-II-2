5abd6eac8db1d49e552fb00297445e6107f07b8d
curiousdave1472 <davidshanlay@gmail.com>
2026-06-12 02:56:28 +0000

    Filter results

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
-// Write your code below
+// Filter the max length from the words.
 
+var words = ["hello", "world", "foo", "bar"];
+
+//Assign the variable for maxLength.
+
+var maxLength = [];
+
+//Create the function to filter the words with length.
+
+function filterStrings(words,len){
+
+
+
+    for (var i = 0; i < 4; i = i +1) {
+        
+
+        if (words[i].length <= len) {
+
+         maxLength.push(words[i]);
+        }
+    }
+    return maxLength
+    
+}
+var results = filterStrings(words, 3);
+console.log(results);
